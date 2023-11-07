93e9346e54d405acd3837b0ddc3103e14a6e4092
Javier Veron <javier.veron@gmail.com>
2023-11-07 01:52:46 -0300

    Crear Migraciones

--- database/migrations/2023_11_07_043934_orders.php
+++ database/migrations/2023_11_07_043934_orders.php
@@ -11,13 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('orders', function (Blueprint $table) {
+            $table->bigInteger("ID")->id();
+            $table->string('order_ref', 255);
+            $table->string('customer_name', 255);
             $table->timestamps();
         });
     }
@@ -27,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('orders');
     }
 };
